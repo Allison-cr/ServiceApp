2227a509e68e18abb3255b08a8b4925dcdf0c584
Allison <suprun-s@bk.ru>
2024-03-27 21:37:56 +0300

    Initial Commit

---
+++ ServiceApp/ServiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceAppApp.swift
+//  ServiceApp
+//
+//  Created by Alexander Suprun on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ServiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
